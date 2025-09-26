adacf9d242c50de003e1a9e26a4b0224d5716a3d
Lorne <dev@luotao.net>
2025-09-26 13:43:45 +0000

    fit colab run

--- setup_env.sh
+++ setup_env.sh
@@ -8,11 +8,17 @@ conda activate latentsync
 conda install -y -c conda-forge ffmpeg
 
 # Python dependencies
+cd /content/LatentSync
 pip install -r requirements.txt
 
 # OpenCV dependencies
 sudo apt -y install libgl1
 
+rm ~/.gitconfig
+ln -s /content/LatentSync/.gitconfig ~/.gitconfig
+
 # Download the checkpoints required for inference from HuggingFace
-huggingface-cli download ByteDance/LatentSync-1.6 whisper/tiny.pt --local-dir checkpoints
-huggingface-cli download ByteDance/LatentSync-1.6 latentsync_unet.pt --local-dir checkpoints
+hf download ByteDance/LatentSync-1.6 whisper/tiny.pt --local-dir checkpoints
+hf download ByteDance/LatentSync-1.6 latentsync_unet.pt --local-dir checkpoints
+
+#./inference.sh
